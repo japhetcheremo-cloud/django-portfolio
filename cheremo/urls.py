a6ff93d88f197c4f6aa2d3bae829b2c7921fea1f
japhetcheremo-cloud <japhetcheremo@gmail.com>
2026-07-25 18:09:18 +0300

    Fix Django admin URL configuration

--- cheremo/urls.py
+++ cheremo/urls.py
@@ -1,39 +1,27 @@
 """
 URL configuration for cheremo project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+The `urlpatterns` list routes URLs to views.
+For more information please see:
+https://docs.djangoproject.com/en/6.0/topics/http/urls/
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # Django Admin
     path("admin/", admin.site.urls),
+
+    # Kenya App
     path("", include("kenya.urls")),
 ]
 
+# Serve media files during development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-from django.urls import path,include
-
-
-urlpatterns=[
-
-path(
-"",
-include("kenya.urls")
-),
-
-]
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
